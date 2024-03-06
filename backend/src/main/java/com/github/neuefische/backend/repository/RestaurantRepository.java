6bce1b0c571f02f698c124a23323c0522caeea46
emea94 <emea94@web.de>
2024-03-06 10:10:30 +0100

    add Annotation '@Repository'

--- backend/src/main/java/com/github/neuefische/backend/repository/RestaurantRepository.java
+++ backend/src/main/java/com/github/neuefische/backend/repository/RestaurantRepository.java
@@ -2,6 +2,8 @@ package com.github.neuefische.backend.repository;
 
 import com.github.neuefische.backend.model.Restaurant;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
 }

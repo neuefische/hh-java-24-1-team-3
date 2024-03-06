3b0905d48bb3a24dfc29705d7cf96456fa2f0e7c
emea94 <emea94@web.de>
2024-03-06 11:23:24 +0100

    setup frontend with router and axios, set default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import {Route, Routes} from "react-router-dom";
+
 export default function App() {
 
   return (
     <>
+      <Routes>
+        <Route path={"/"} element={<h1>Hello Foodies!</h1>}/>
+      </Routes>
     </>
   )
 }

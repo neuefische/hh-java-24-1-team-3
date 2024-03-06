d1a790b7ab1aa2ca9832a580b2f9d24dd5de8787
Eva <73281708+emea94@users.noreply.github.com>
2024-03-06 10:46:18 +0100

    Update backend/src/main/java/com/github/neuefische/backend/controller/RestaurantController.java
    Co-authored-by: Marcell Dechant <marcell.dechant@neuefische.de>

--- backend/src/main/java/com/github/neuefische/backend/controller/RestaurantController.java
+++ backend/src/main/java/com/github/neuefische/backend/controller/RestaurantController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/restaurant")
+@RequestMapping("/api/restaurants")
 @RequiredArgsConstructor
 public class RestaurantController {
 

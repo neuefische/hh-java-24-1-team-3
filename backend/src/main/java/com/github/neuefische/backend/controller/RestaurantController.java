d78b0adaf754edd22d14a3e143edda98ecc690a6
emea94 <emea94@web.de>
2024-03-06 11:55:56 +0100

    create a GET "/api/restaurants" endpoint to fetch all restaurants from the backend

--- backend/src/main/java/com/github/neuefische/backend/controller/RestaurantController.java
+++ backend/src/main/java/com/github/neuefische/backend/controller/RestaurantController.java
@@ -1,9 +1,13 @@
 package com.github.neuefische.backend.controller;
 
+import com.github.neuefische.backend.model.Restaurant;
 import com.github.neuefische.backend.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/restaurants")
@@ -12,4 +16,8 @@ public class RestaurantController {
 
     private final RestaurantService service;
 
+    @GetMapping
+    public List<Restaurant> getAllRestaurants() {
+        return service.getAllRestaurants();
+    }
 }

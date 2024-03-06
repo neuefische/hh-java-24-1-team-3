1e4d8099ce938de7f7e4a332779a67bff1063704
emea94 <emea94@web.de>
2024-03-06 11:55:56 +0100

    create a GET "/api/restaurants" endpoint to fetch all restaurants from the backend

--- backend/src/main/java/com/github/neuefische/backend/model/Restaurant.java
+++ backend/src/main/java/com/github/neuefische/backend/model/Restaurant.java
@@ -2,7 +2,7 @@ package com.github.neuefische.backend.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document("restaurants")
 public record Restaurant(
         String id,
         String title,
--- backend/src/main/java/com/github/neuefische/backend/service/RestaurantService.java
+++ backend/src/main/java/com/github/neuefische/backend/service/RestaurantService.java
@@ -1,12 +1,19 @@
 package com.github.neuefische.backend.service;
 
+import com.github.neuefische.backend.model.Restaurant;
 import com.github.neuefische.backend.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantService {
 
     private final RestaurantRepository repo;
+
+    public List<Restaurant> getAllRestaurants() {
+        return repo.findAll();
+    }
 }

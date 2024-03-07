48c85cd0779f90936917ff494c4ead7fdfe2a6cb
ingobecker <mail@ingobecker.de>
2024-03-07 12:57:34 +0100

    implement a component that shows the restaurant Details when you click on restaurant in the list view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import {Route, Routes} from "react-router-dom";
 import RestaurantOverview from "./components/RestaurantOverview.tsx";
+import RestaurantDetail from "./components/RestaurantDetail.tsx";
 
 export default function App() {
 
   return (
     <>
       <Routes>
-        <Route path={"/"} element={<RestaurantOverview/>}/>
+          <Route path={"/"} element={<RestaurantOverview/>}/>
+          <Route path={"/restaurants/:id"} element={<RestaurantDetail/>}/>
       </Routes>
-
-
     </>
   )
 }

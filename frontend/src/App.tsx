875160ae0d5b179f023baf46162d2e9876458d3b
ingobecker <mail@ingobecker.de>
2024-03-08 16:24:48 +0100

    add header with title, logo,  searchbar and link to 'Add Restaurant'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,61 @@
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 import RestaurantOverview from "./components/RestaurantOverview.tsx";
 import NewRestaurantForm from "./components/NewRestaurantForm.tsx";
 import RestaurantDetail from "./components/RestaurantDetail.tsx";
+import "./App.css"
+import {useEffect, useState} from "react";
+import {Restaurant} from "./types/Restaurant.ts";
+import axios from "axios";
+import SearchComponent from "./components/SearchComponent.tsx";
+
 
 export default function App() {
+    const [restaurants, setRestaurants] = useState<Restaurant[]>([])
+    const [searchText, setSearchText] = useState<string>("")
+
+    const filteredRestaurants = restaurants.filter(restaurant => restaurant.title.toLowerCase().includes(searchText.toLowerCase())
+        || restaurant.city.toLowerCase().includes(searchText.toLowerCase()))
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        axios.get('/api/restaurants')
+            .then(response => {
+                setRestaurants(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching Restaurants: ', error);
+            });
+    }
 
   return (
     <>
-      <Routes>
-        <Route path={"/"} element={<RestaurantOverview/>}/>
-          <Route path={"/restaurants/add"} element={<NewRestaurantForm/>}/>
+        <header>
+            <div className={"HeaderComponent"}>
+                <div className={"Wrapper"}>
+                    <Link to={"/"}>
+                        <img className={"Logo"} src={"foodelicious.png"}
+                             alt={"Bild von einem Burger mit Getränk"}/>
+                        <h1>Foodelicious</h1>
+                    </Link>
+                    <Link to={"/restaurants/add"}>Add Restaurant</Link>
+                </div>
+                <SearchComponent handleSearchText={setSearchText}/>
+            </div>
+        </header>
+        <Routes>
+            <Route path={"/"} element={
+                filteredRestaurants.length > 0
+                    ?
+                    <RestaurantOverview restaurants={filteredRestaurants}/>
+                    :
+                    <h1>No restaurants found</h1>
+            }/>
           <Route path={"/restaurants/:id"} element={<RestaurantDetail/>}/>
+            <Route path={"/restaurants/add"} element={<NewRestaurantForm/>}/>
       </Routes>
-
-
     </>
   )
 }
--- frontend/src/components/RestaurantOverview.tsx
+++ frontend/src/components/RestaurantOverview.tsx
@@ -1,29 +1,14 @@
-import {useEffect, useState} from "react";
 import {Restaurant} from "../types/Restaurant.ts";
-import axios from "axios";
 import RestaurantCard from "./RestaurantCard.tsx";
 
-export default function RestaurantOverview() {
-
-    const [restaurants, setRestaurants] = useState<Restaurant[]>([])
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const fetchData = () => {
-        axios.get('/api/restaurants')
-            .then(response => {
-                setRestaurants(response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching Restaurants: ', error);
-            });
-    }
+type RestaurantOverviewProps = {
+    restaurants: Restaurant[]
+}
+export default function RestaurantOverview(props: Readonly<RestaurantOverviewProps>) {
 
     return (
         <div>
-            {restaurants.map(restaurant => <RestaurantCard key={restaurant.id} restaurant={restaurant}/>)}
+            {props.restaurants.map(restaurant => <RestaurantCard key={restaurant.id} restaurant={restaurant}/>)}
         </div>
     );
 }

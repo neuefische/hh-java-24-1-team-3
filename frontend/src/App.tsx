2b6cc77adf9d2fb0afd95a4fe6bda76076691532
emea94 <emea94@web.de>
2024-03-08 14:36:09 +0100

    create a "Add restaurant" button in the UI implement a form to input restaurant details add some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import {Route, Routes} from "react-router-dom";
 import RestaurantOverview from "./components/RestaurantOverview.tsx";
+import NewRestaurantForm from "./components/NewRestaurantForm.tsx";
 import RestaurantDetail from "./components/RestaurantDetail.tsx";
 
 export default function App() {
@@ -7,9 +8,12 @@ export default function App() {
   return (
     <>
       <Routes>
-          <Route path={"/"} element={<RestaurantOverview/>}/>
+        <Route path={"/"} element={<RestaurantOverview/>}/>
+          <Route path={"/restaurants/add"} element={<NewRestaurantForm/>}/>
           <Route path={"/restaurants/:id"} element={<RestaurantDetail/>}/>
       </Routes>
+
+
     </>
   )
 }

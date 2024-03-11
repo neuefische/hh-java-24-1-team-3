8af8823988e38263ca3338eafd889731da78079e
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-11 15:53:34 +0100

    update NewRestaurantForm, implement YUP

--- frontend/src/components/NewRestaurantForm.tsx
+++ frontend/src/components/NewRestaurantForm.tsx
@@ -1,31 +1,97 @@
 import {FormEvent, useState} from "react";
 import "./NewRestaurantForm.css"
 import axios from "axios";
+import * as yup from "yup";
 import {useNavigate} from "react-router-dom";
+import {RestaurantAddress} from "../types/Restaurant.ts";
 
+type Input = {
+    title: string,
+    city: string,
+    cuisine: string,
+    address: RestaurantAddress
+}
+
+const initialErrorState = {
+    title: '',
+    city: '',
+    cuisine: '',
+    address: {
+        address: '',
+        number: ''
+    }
+};
+
+const initialFormValue = {title: "", city: "", cuisine: "", address: {address: "", number: ""}}
+
+const formDataSchema = yup.object().shape({
+    title: yup.string().required('Title is required').min(3, 'Title must be at least 3 characters').max(100, 'Title must be at most 100 characters'),
+    city: yup.string().required('City is required').min(3, 'City must be at least 3 characters').max(100, 'City must be at most 100 characters'),
+    cuisine: yup.string().required('Cuisine is required').min(3, 'Cuisine must be at least 3 characters').max(50, 'Cuisine must be at most 50 characters'),
+    address: yup.object().shape({
+        address: yup.string().required('Address is required').min(3, 'Address must be at least 3 characters').max(100, 'Address must be at most 100 characters'),
+        number: yup.string().required('Number is required').min(1, 'Number must be at least 1 character').max(5, 'Number must be at most 5 characters').matches(/^'\d'+$/, 'Number must be a number')
+    })
+});
 
 export default function NewRestaurantForm() {
 
-    const [title, setTitle] = useState("")
-    const [city, setCity] = useState("")
+    const [formData, setFormData] = useState<Input>(initialFormValue)
+    const [submittedFormData, setSubmittedFormData] = useState<Input[]>([])
+    const [errors, setErrors] = useState(initialErrorState)
     const navigate = useNavigate()
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        navigate("/")
-    };
-
-    function saveNewRestaurant() {
-        setCity("")
-        setTitle("")
-        axios.post("/api/restaurants",
-            {
-                title: title,
-                city: city
-        })
-
+        formDataSchema.validate(formData, {abortEarly: false})
+            .then(() => {
+                saveNewRestaurant();
+                setFormData(initialFormValue)
+                setErrors(initialErrorState);
+            })
+            .catch((validationErrors: yup.ValidationError) => {
+                const errors: {
+                    title: string;
+                    city: string;
+                    cuisine: string;
+                    address: { address: string; number: string; };
+                } = {
+                    title: '',
+                    city: '',
+                    cuisine: '',
+                    address: {
+                        address: '',
+                        number: ''
+                    }
+                };
+                validationErrors.inner.forEach((currentError) => {
+                    if (currentError.path === 'address.address' || currentError.path === 'address.number') {
+                        if (!errors.address) {
+                            errors.address = {
+                                address: '',
+                                number: ''
+                            };
+                        }
+                        // @ts-ignore
+                        errors.address[currentError.path.split('.')[1]] = currentError.message;
+                    } else {
+                        // @ts-ignore
+                        errors[currentError.path] = currentError.message;
+                    }
+                });
+                setErrors(errors);
+            });
     }
 
+    function saveNewRestaurant() {
+        axios.post("/api/restaurants", formData)
+            .then((response) => {
+                console.log(response.data)
+                setSubmittedFormData([...submittedFormData, formData])
+                navigate("/")
+            })
+            .catch((error) => console.log(error.message))
+    }
 
     return (
         <form onSubmit={handleSubmit} >
@@ -33,18 +99,59 @@ export default function NewRestaurantForm() {
             <label>Title:
                 <input
                     type={"text"}
-                    value={title}
-                    onChange={event => setTitle(event.target.value)}
+                    value={formData.title}
+                    onChange={event =>
+                        setFormData({...formData, title: event.target.value})}
+                    maxLength={100}
+                    required={true}
                 />
+                {errors.title && <p>{errors.title}</p>}
             </label>
             <label>City:
                 <input
                     type={"text"}
-                    value={city}
-                    onChange={event => setCity(event.target.value)}
+                    value={formData.city}
+                    onChange={event => setFormData({...formData, city: event.target.value})}
+                    maxLength={100}
+                    required={true}
                 />
             </label>
-            <button onClick={saveNewRestaurant}>Add restaurant</button>
+            <label>Cuisine:
+                <input
+                    type={"text"}
+                    value={formData.cuisine}
+                    onChange={event => setFormData({...formData, cuisine: event.target.value})}
+                    maxLength={50}
+                    required={true}
+                />
+            </label>
+            <div className={"address-wrapper"}>
+                <label>Address:
+                    <input
+                        type={"text"}
+                        value={formData.address.address}
+                        onChange={event => setFormData({
+                            ...formData,
+                            address: {...formData.address, address: event.target.value}
+                        })}
+                        maxLength={100}
+                        required={true}
+                    />
+                </label>
+                <label>Number:
+                    <input
+                        type={"text"}
+                        value={formData.address.number}
+                        onChange={event => setFormData({
+                            ...formData,
+                            address: {...formData.address, number: event.target.value}
+                        })}
+                        maxLength={5}
+                        required={true}
+                    />
+                </label>
+            </div>
+            <button type={"submit"}>Add restaurant</button>
         </form>
     );
 }

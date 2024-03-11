ee7f4731dcad11d8fad82b1e32ddde8cee1363b7
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-11 12:24:13 +0100

    update type Restaurant.ts

--- frontend/src/types/Restaurant.ts
+++ frontend/src/types/Restaurant.ts
@@ -2,4 +2,11 @@ export type Restaurant = {
     id: string,
     title: string,
     city: string,
+    cuisine: string,
+    address: RestaurantAddress
+}
+
+export type RestaurantAddress = {
+    address: string,
+    number: string
 }

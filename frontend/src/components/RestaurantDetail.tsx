c4a17e26ada8d4fcd0f9bad77f6d936cc72ea4f6
ingobecker <mail@ingobecker.de>
2024-03-12 17:24:19 +0100

    add edit function to frontend

--- frontend/src/components/RestaurantDetail.tsx
+++ frontend/src/components/RestaurantDetail.tsx
@@ -8,9 +8,21 @@ type RestaurantDetailProps = {
     saveNewRestaurant: React.Dispatch<React.SetStateAction<Restaurant[]>>,
     restaurants: Restaurant[]
 }
+
+type Input = {
+    title: string,
+    city: string,
+    cuisine: string,
+    address: RestaurantAddress
+}
+
+const initialFormValue = {title: "", city: "", cuisine: "", address: {address: "", number: ""}}
+
 export default function RestaurantDetail(props: Readonly<RestaurantDetailProps>) {
     const params = useParams()
     const [restaurantAddress, setRestaurantAddress] = useState<RestaurantAddress>({address: "", number: ""});
+    const [isEditable, setIsEditable] = useState<boolean>(false);
+    const [formData, setFormData] = useState<Input>(initialFormValue)
     const [restaurant, setRestaurant] = useState<Restaurant>({
         id: "",
         title: "",
@@ -22,6 +34,30 @@ export default function RestaurantDetail(props: Readonly<RestaurantDetailProps>)
 
     function navigateToHome(){
         navigate("/")
+    }
+
+    function handleEdit() {
+        setIsEditable(true)
+        setFormData({
+            title: restaurant.title,
+            city: restaurant.city,
+            cuisine: restaurant.cuisine,
+            address: restaurant.address
+        })
+    }
+
+    function handleCancel() {
+        setIsEditable(false)
+    }
+
+    function handleEditSave() {
+        axios.put("/api/restaurants/" + params.id, formData)
+            .then((response) => {
+                    setIsEditable(false)
+                    setRestaurant(response.data)
+                }
+            )
+            .catch((error) => console.log(error.message))
     }
 
     function deleteRestaurant(){
@@ -48,20 +84,78 @@ export default function RestaurantDetail(props: Readonly<RestaurantDetailProps>)
         [params.id]
     )
 
+
     return (
         <div className={"DetailRestaurantPage"}>
             <h1>Details:</h1>
             <div className={"RestaurantDetail"}>
-                <h2>{restaurant.title}</h2>
-                <h3>{restaurant.city}</h3>
-                <h3>{restaurant.cuisine}</h3>
-                <h3>{restaurant.address.address}</h3>
-                <h3>{restaurant.address.number}</h3>
+                {isEditable
+                    ? (
+                        <div>
+                            <input
+                                type={"text"}
+                                value={formData.title}
+                                onChange={event =>
+                                    setFormData({...formData, title: event.target.value})}
+                                maxLength={100}
+                                required={true}
+                            />
+                            <input
+                                type={"text"}
+                                value={formData.city}
+                                onChange={event => setFormData({...formData, city: event.target.value})}
+                                maxLength={100}
+                                required={true}
+                            />
+                            <input
+                                type={"text"}
+                                value={formData.cuisine}
+                                onChange={event => setFormData({...formData, cuisine: event.target.value})}
+                                maxLength={50}
+                                required={true}
+                            />
+                            <input
+                                type={"text"}
+                                value={formData.address.address}
+                                onChange={event => setFormData({
+                                    ...formData,
+                                    address: {...formData.address, address: event.target.value}
+                                })}
+                                maxLength={100}
+                                required={true}
+                            />
+                            <input
+                                type={"text"}
+                                value={formData.address.number}
+                                onChange={event => setFormData({
+                                    ...formData,
+                                    address: {...formData.address, number: event.target.value}
+                                })}
+                                maxLength={5}
+                                required={true}
+                            />
+                            <div>
+                                <button onClick={handleEditSave}>Save</button>
+                                <button onClick={handleCancel}>Cancel</button>
+                            </div>
+                        </div>
+                    )
+                    :
+                    (
+                        <div>
+                            <h2>{restaurant.title}</h2>
+                            <h3>{restaurant.city}</h3>
+                            <h3>{restaurant.cuisine}</h3>
+                            <h3>{restaurant.address.address}</h3>
+                            <h3>{restaurant.address.number}</h3>
+                        </div>
+                    )
+                }
             </div>
             <div className={"ButtonWrapper"}>
-            <button className="HomeButton" onClick={navigateToHome}>Back</button>
-                <button className="HomeButton"
-                        onClick={() => navigate("/restaurants/" + restaurant.id + "/edit")}>Edit
+                <button className="HomeButton" onClick={navigateToHome}>Back</button>
+                <button className="EditButton"
+                        onClick={handleEdit}>Edit
                 </button>
                 <button className="DeleteButton" onClick={deleteRestaurant}>Delete</button>
             </div>

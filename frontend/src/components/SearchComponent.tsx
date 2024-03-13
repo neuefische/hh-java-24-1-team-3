57bfd061bb9e7f9b82d13c71af81087fcec1dbcd
emea94 <emea94@web.de>
2024-03-13 18:12:03 +0100

    add styling for SearchComponent try install AwesomeFonts

--- frontend/src/components/SearchComponent.tsx
+++ frontend/src/components/SearchComponent.tsx
@@ -1,4 +1,7 @@
 import {ChangeEvent} from "react";
+import "./SearchComponent.css";
+import {FontAwesomeIcon} from "@fortawesome/fontawesome-free";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 
 type SearchComponentProps = {
     handleSearchText: (searchText: string) => void,
@@ -10,6 +13,9 @@ export default function SearchComponent(props: Readonly<SearchComponentProps>) {
     }
 
     return (
-        <input onChange={handleInputOnChange}/>
+        <div className="search-box">
+            <button className="btn-search"><FontAwesomeIcon icon={faSearch}/></button>
+            <input onChange={handleInputOnChange} type="text" className="input-search" placeholder="Type to Search..."/>
+        </div>
     );
 }

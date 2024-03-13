28dca639169581ec35e1914ab78a03273ec985b0
Jana Schmälzle <jana@familie-schmaelzle.de>
2024-03-13 19:48:00 +0100

    fix overview bottom css

--- frontend/src/components/RestaurantOverview.tsx
+++ frontend/src/components/RestaurantOverview.tsx
@@ -10,10 +10,12 @@ export default function RestaurantOverview(props: Readonly<RestaurantOverviewPro
 
 
     return (
-        <div className={"restaurant-grid"}>
-            {props.restaurants.map(restaurant =>
-                <RestaurantCard key={restaurant.id} restaurant={restaurant}/>)}
+        <div className={"restaurant-grid-container"}>
+            <div className={"restaurant-grid"}>
+                {props.restaurants.map(restaurant =>
+                    <RestaurantCard key={restaurant.id} restaurant={restaurant}/>)}
+            </div>
         </div>
-    );
+    )
 }
 
